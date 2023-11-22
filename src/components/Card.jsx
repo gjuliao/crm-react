4412fc7421cc6f534d66a24d1a82f130604f07d2
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-22 17:33:19 -0500

    Added styles to card

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className='group-card'>
+        <h2>Title</h2>
+        <div className='items'>
+            <p>Left</p>
+            <p>Item One</p>
+            <p>Right</p>
+        </div>
+    </div>
+  )
+}
