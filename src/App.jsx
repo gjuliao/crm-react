19ea6c8b3f1ae04f522bad9d6463fe18230d7101
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-22 20:14:34 -0500

    Added more props to cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,29 @@ import { Card } from './components/Card';
 
 function App() {
 
-  const [list1, setList1] = useState(['task1', 'task2']);
+  const [list1, setList1] = useState( 
+    {
+      'owner': 'Brad',
+      'tasks': ['task1', 'task2']
+    });
+
+    const [list2, setList2] = useState( 
+      {
+        'owner': 'Giselle',
+        'tasks': ['task3', 'task4', 'task5']
+      });
+
+      const [list3, setList3] = useState( 
+        {
+          'owner': 'Lorenzo',
+          'tasks': ['task6', 'task7', 'task8', 'task9']
+        });
+
+        const [list4, setList4] = useState( 
+          {
+            'owner': 'Mike',
+            'tasks': ['task10', 'task11']
+          });
 
   const handleItemClick = () => {
     // setList1((el) => setList.filter((i) => i !== list1));
@@ -15,9 +37,9 @@ function App() {
   return (
     <div className="container">
       <Card items={list1} onItemClick={handleItemClick} />
-      <Card />
-      <Card />
-      <Card />
+      <Card items={list2} onItemClick={handleItemClick}/>
+      <Card items={list3} onItemClick={handleItemClick}/>
+      <Card items={list4} onItemClick={handleItemClick}/>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Items } from './Items'
 
-export const Card = ({ items = [], onItemClick }) => {
+export const Card = ({ items, onItemClick }) => {
+
+    const tasks = items.tasks;
+    const owner = items.owner;
 
     console.log(items, 'Card');
   return (
     <div className='group-card'>
-        <h2 className='card-title'>Title</h2>
+        <h2 className='card-title'>{owner}</h2>
         <div>
-        {items.map((item, index) => (
+        {tasks.map((item, index) => (
             <Items key={index} item={item} />
         ))}
-            <Items />
-            <Items />
         </div>
         <button>submit</button>
     </div>
